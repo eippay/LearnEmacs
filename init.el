5aec0936dba3846a2871b01b714e82d968ebbba3
thinkpad_X200 <eippay@163.com>
2018-06-14 16:49:59 +0800

    将插件相关移动到lisp/init-packages中

--- init.el
+++ init.el
@@ -1,98 +1,9 @@
+(add-to-list 'load-path "~/.emacs.d/lisp/")
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;; (package-initialize)
+(require 'init-packages)
 
-;;####################
-;; zilongshanren源
-;;####################
-;; 添加melpa源
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
-(package-initialize)
-
-
-;; 导入 common lisp 库
-
-(require 'cl)
-
-;;(defvar zilongshanren/packages nil)
-(setf *install-packages* '(
-				 company
-				 monokai-theme
-				 hungry-delete
-;;				 smex
-				 swiper
-				 counsel
-				 smartparens
-				 js2-mode
-				 nodejs-repl
-					 ))
-(setq package-selected-packages *install-packages*)
-(let ((installing-packages-list (remove-if #'(lambda (pkg) (package-installed-p pkg)) *install-packages*)))
-  (when installing-packages-list
-    (message "%s" "Refreshing package database...")
-   (package-refresh-contents)
-    (dolist (pkg installing-packages-list)
-      (package-install pkg))))
-
-;;(defun zilongshanren/packages-installed-p ()
-  ;(loop for pkg in zilongshanren/packages
-;;	when (not (package-installed-p pkg)) do (return nil)
-;;	finally (return t)))
-
-;;(unless (zilongshanren/packages-installed-p)
-;;  (message "%s" "Refreshing package database...")
-;;  (package-refresh-contents)
-;;  (dolist (pkg zilongshanren/packages)
-;;    (when (not (package-installed-p pkg))
-;;      (package-install pkg))))
-;;####################
-;; smartparens
-;;####################
-(require 'smartparens-config)
-;; (add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
-;; 全局使用smartparens
-(smartparens-global-mode t)
-
-;;####################
-;; swiper插件
-;;####################
-
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-h f") 'counsel-describe-function)
-(global-set-key (kbd "C-h v") 'counsel-describe-variable)
-
-;;####################
-;; 使用 js2-mode 打开 .js 文件
-(setq auto-mode-alist
-      (append
-       '(("\\.js\\'" . js2-mode))
-       auto-mode-alist))
-(require 'nodejs-repl)
-
-;;####################
-;; 外观设置
-;;####################
-;; 加载主题
-(load-theme 'monokai t)
+;; 关闭声音提示
+(setq ring-bell-function 'ignore)
 ;; 关闭工具栏
 (tool-bar-mode 0)
 ;; 关闭侧栏
@@ -127,10 +38,10 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 
 ;; 显示最近打开的文件
-(require 'recentf)
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+;;(require 'recentf)
+;;(recentf-mode 1)
+;;(setq recentf-max-menu-items 25)
+;;(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 ;; 禁止生成备份文件
 (setq make-backup-files nil)
 ;; 文本中语法高亮
@@ -141,9 +52,7 @@
 
 ;; 输入时删除选中的文本
 ;; (delete-selection-mode t)
-;; 贪婪删除多余空白
-(require 'hungry-delete)
-(global-hungry-delete-mode)
+
 ;; 高亮当前行
 (global-hl-line-mode t)
 
@@ -156,17 +65,8 @@
 ;; 在 [File] => [File Recent] 里保存最近打开文件
 ;; (recentf-mode t)
 
-;; 全局使用补全模式
-(global-company-mode t)
-;; smex补全M-x
-;;(require 'smex) ; Not needed if you use package.el
-;;(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
-					; when Smex is auto-initialized on its first run.
-;;(global-set-key (kbd "M-x") 'smex)
-;;(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; This is your old M-x.
-;;(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
- 
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ lisp/init-packages.el
@@ -0,0 +1,121 @@
+
+;; 添加melpa源
+;; 这是直接从https://melpa.org/#/getting-started复制的
+(progn
+  (require 'package)
+  (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                      (not (gnutls-available-p))))
+	 (proto (if no-ssl "http" "https")))
+    ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+    (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+    ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+    (when (< emacs-major-version 24)
+      ;; For important compatibility libraries like cl-lib
+      (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+  (package-initialize)
+)
+
+;; 定义 eippay/package-installing 用于安装扩展包
+(progn
+  ;; 导入 common lisp 库
+  (require 'cl)
+  ;; package-auto-remove 会清理 package-selected-packages 中没有的插件
+  (setq package-selected-packages nil)
+  ;; 当插件未安装时，则安装插件
+  (defun eippay/package-installing (pkg &optional dependent-pkg)
+    (if dependent-pkg
+	(eippay/package-installing dependent-pkg))
+    (pushnew pkg package-selected-packages)
+    (unless (package-installed-p pkg)
+      (message "%s" "Refreshing package database...")
+      (package-refresh-contents)
+      (message "installing %s ..." pkg)
+      (package-install pkg)))
+
+  (setf *keybind-list* nil)
+  (defun eippay/keybind (string key fn)
+   (pushnew (concatenate 'string ";; " string) *keybind-list*)
+   (pushnew (concatenate 'string "global-set-key (kbd " key ") " (format nil "'~a" fn)) *keybind-list*))
+  )
+;;####################
+;; company 用于自动补全
+;;####################
+(eippay/package-installing 'company)
+;; 全局使用补全模式
+(global-company-mode t)
+
+;;####################
+;; monokai-theme 提供主题
+;;####################
+(eippay/package-installing 'monokai-theme)
+;; 加载主题
+(load-theme 'monokai t)
+
+;;####################
+;; hungry-delete 贪婪删除多余空白
+;;####################
+(eippay/package-installing 'hungry-delete)
+;; 全局开启贪婪删除空白
+(global-hungry-delete-mode)
+
+;;####################
+;; swiper
+;;####################
+;; 优化minibuffer
+;; swiper 依赖于 counsel
+(eippay/package-installing 'swiper 'counsel)
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+;;####################
+;; smartparens
+;;####################
+;; 自动为"("输入匹配的")"
+(eippay/package-installing 'smartparens)
+(require 'smartparens-config)
+;; 全局使用smartparens
+(smartparens-global-mode t)
+
+
+;;####################
+;; js2-mode
+;; nodejs-repl
+;;####################
+;; javascript IDE
+(eippay/package-installing 'js2-mode)
+(eippay/package-installing 'nodejs-repl)
+;; 使用 js2-mode 打开 .js 文件
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+(require 'nodejs-repl)
+
+;;####################
+;; popwin
+;;####################
+;; 光标自动跳转到提示窗口
+(eippay/package-installing 'popwin)
+(require 'popwin)
+(popwin-mode t)
+
+
+
+;;####################
+;; 清除未使用的插件
+;;####################
+(package-autoremove)
+
+;;####################
+;; 添加快捷键
+;;####################
+
+(provide 'init-packages)
